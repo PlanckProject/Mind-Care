b6800ff1b7d5e4ba92ca1a8d8d4fbe643d66945b
Gaurav Saini <sainigaurav.2009@gmail.com>
2021-03-02 18:23:46 +0530

    Fixed website name and added background

--- frontend/components/SiteHeader.vue
+++ frontend/components/SiteHeader.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
-    <a href="/"><h1>My cool website title! :D</h1></a>
+    <a href="/"><h1>Mind Care</h1></a>
   </div>
 </template>
 
 <style lang="scss" scoped>
 div {
+  background-image: url('~assets/img/leaf.png');
   padding: 1rem;
   border-radius: 0 0 0.5rem 0.5rem;
   text-align: center;
