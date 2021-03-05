3448c5c9bb4e52b8bd78f0f3b3cf845f08214b81
Gaurav Saini <sainigaurav.2009@gmail.com>
2021-03-06 03:39:51 +0530

    Added /register link to redirect to Google form

---
+++ frontend/pages/register.vue
@@ -0,0 +1,9 @@
+<template>
+</template>
+<script>
+export default {
+    created() {
+        window.location = "https://docs.google.com/forms/d/e/1FAIpQLSepDnZf5RHChv3kbEyq5cIYgzy06IDL1imzKedwj0coXHHBoA/viewform";
+    }
+}
+</script>
