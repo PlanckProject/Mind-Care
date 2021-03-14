145b30b7727e2644c64c0df77df0e08ca2bb998a
Gaurav Saini <sainigaurav.2009@gmail.com>
2021-03-14 18:06:09 +0530

    Added text justification and fixed an issue with services list

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -13,8 +13,8 @@
           No matching service providers were found near your current location.
           This doesn't look like an issue to us, but the non-availability of
           services around your current location. Please get in touch with the
-          team at <a href="mailto:wb.res@outlook.com">wb.res@outlook.com</a> for
-          registering new service providers around you or look for an online
+          team at <a href="mailto:wb.res@outlook.com">wb.res@outlook.com</a> or click <a href="/register">here</a> for
+          registering new service providers around you or look for online
           service providers by clicking <a href="/">here</a>.
         </p>
         <br />
